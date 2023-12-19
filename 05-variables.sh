ad560cf19aa32b35278bc30f1a986f1e81393590
suriv <info@sagakumudini.com>
2023-12-19 14:27:06 -0600

    added 6th script

--- 05-variables.sh
+++ 05-variables.sh
@@ -1,7 +1,13 @@
 #!/bin/bash
-echo "Please enter your username"
+echo "Please enter your username::"
 
 read  -s USERNAME # the vale entered above will be automatically attached to the username variable
 
 
-echo "Username is: $USERNAME"
+#just for reference but shouldnt do in rall scripts
+
+echo "Please enter your password::"
+
+read  -s PASSWORD
+
+echo "Username is: $USERNAME, Password is: $PASSWORD"
