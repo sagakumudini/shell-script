06c5cc3ac10574a6b0785227f503d787be7b5b6e
suriv <info@sagakumudini.com>
2023-12-19 14:11:49 -0600

    added 5th script

--- 05-variables.sh
+++ 05-variables.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 echo "Please enter your username"
 
-read USERNAME # the vale entered above will be automatically attached to the username variable
+read  -s USERNAME # the vale entered above will be automatically attached to the username variable
 
 
 echo "Username is: $USERNAME"
